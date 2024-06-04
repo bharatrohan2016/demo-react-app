33b5afd6db5a85e8b649ce679ea062b15b103c07
sanu-coder <sanya30122000@gmail.com>
2024-06-04 17:44:26 +0530

    commit

---
+++ src/Pages/States/States.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const States = () => {
+    
+    return (
+        <div>
+            <h4>States</h4>
+           
+            <div class="row gx-5">
+                <div class="col">
+                    
+                    <div class="p-3 border bg-light text-center"><p>Maharashtra</p></div>
+                </div>
+
+                <div class="col ">
+                    <div class="p-3 border bg-light text-center"><p>Rajasthan</p></div>
+                </div>
+
+                <div class="col ">
+                    <div class="p-3 border bg-light text-center">
+                        <p>Uttar Pradesh</p>
+                    </div>
+                </div>
+
+                <div class="col ">
+                    <div class="p-3 border bg-light text-center"><p>Haryana</p></div>
+                </div>
+
+                <div class="col ">
+                    <div class="p-3 border bg-light text-center"><p>Gujrat</p></div>
+                </div>
+            </div>
+           
+        </div>
+    )
+}
+
+export default States;
