7a85f82a404986189ecaf21ec2f7fd7053bd9daa
sanu-coder <sanya30122000@gmail.com>
2024-06-05 15:39:31 +0530

    commit

--- src/Pages/States/States.js
+++ src/Pages/States/States.js
@@ -6,62 +6,62 @@ const States = () => {
         <div style={{height: '80vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-around'}}>
            
             <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
-                <div class="card" style={{width: '15rem', height: '15rem'}}>
-                    <img src="/rajastan.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem'}}/>
+                <div class="card" style={{width: '15rem', height: '15rem', borderRadius : '18px'}}>
+                    <img src="/rajastan.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem', borderRadius : '18px'}}/>
                         <div class="card-body">
                             <h5 class="card-title">Jodhpur: Bilara</h5>
-                            <a href="#" class="btn btn-bluish-green">Go To Map</a>
+                            <a href="#" class="btn btn-rounded btn-bluish-green">Go To Map</a>
                         </div>
                 </div>
-                <div class="card" style={{width: '15rem', height: '15rem'}}>
-                    <img src="/maharashtra.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem'}}/>
+                <div class="card" style={{width: '15rem', height: '15rem', borderRadius : '18px'}}>
+                    <img src="/maharashtra.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem', borderRadius : '18px'}}/>
                         <div class="card-body">
                             <h5 class="card-title">Solapur</h5>
-                            <a href="#" class="btn btn-bluish-green">Go To Map</a>
+                            <a href="#" class="btn btn-rounded btn-bluish-green">Go To Map</a>
                         </div>
                 </div>
-                <div class="card" style={{width: '15rem', height: '15rem'}}>
-                    <img src="/harayana.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem'}}/>
+                <div class="card" style={{width: '15rem', height: '15rem', borderRadius : '18px'}}>
+                    <img src="/harayana.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem', borderRadius : '18px'}}/>
                         <div class="card-body">
                             <h5 class="card-title">Barabanki</h5>
-                            <a href="#" class="btn btn-bluish-green">Go To Map</a>
+                            <a href="#" class="btn btn-rounded btn-bluish-green">Go To Map</a>
                         </div>
                 </div>
-                <div class="card" style={{width: '15rem', height: '15rem'}}>
-                    <img src="/gujrat.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem'}}/>
+                <div class="card" style={{width: '15rem', height: '15rem', borderRadius : '18px'}}>
+                    <img src="/gujrat.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem', borderRadius : '18px'}}/>
                         <div class="card-body">
                             <h5 class="card-title">Beawer</h5>
-                            <a href="#" class="btn btn-bluish-green">Go To Map</a>
+                            <a href="#" class="btn btn-rounded btn-bluish-green">Go To Map</a>
                         </div>
                 </div>
             </div>
            <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
-                <div class="card" style={{width: '15rem', height: '15rem'}}>
-                    <img src="/maharashtra.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem'}}/>
+                <div class="card" style={{width: '15rem', height: '15rem', borderRadius : '18px'}}>
+                    <img src="/maharashtra.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem', borderRadius : '18px'}}/>
                         <div class="card-body">
                             <h5 class="card-title">Nagaur</h5>
-                            <a href="#" class="btn btn-bluish-green">Go To Map</a>
+                            <a href="#" class="btn btn-rounded btn-bluish-green">Go To Map</a>
                         </div>
                 </div>
-                <div class="card" style={{width: '15rem', height: '15rem'}}>
-                    <img src="/harayana.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem'}}/>
+                <div class="card" style={{width: '15rem', height: '15rem', borderRadius : '18px'}}>
+                    <img src="/harayana.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem', borderRadius : '18px'}}/>
                         <div class="card-body">
                             <h5 class="card-title">Barmer</h5>
-                            <a href="#" class="btn btn-bluish-green">Go To Map</a>
+                            <a href="#" class="btn btn-rounded btn-bluish-green">Go To Map</a>
                         </div>
                 </div>
-                <div class="card" style={{width: '15rem', height: '15rem'}}>
-                    <img src="/gujrat.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem'}}/>
+                <div class="card" style={{width: '15rem', height: '15rem', borderRadius : '18px'}}>
+                    <img src="/gujrat.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem', borderRadius : '18px'}}/>
                         <div class="card-body">
                             <h5 class="card-title">Jaisalmer</h5>
-                            <a href="#" class="btn btn-bluish-green">Go To Map</a>
+                            <a href="#" class="btn btn-rounded btn-bluish-green">Go To Map</a>
                         </div>
                 </div>
-                <div class="card" style={{width: '15rem', height: '15rem'}}>
-                    <img src="/uttarpradesh.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem'}} />
+                <div class="card" style={{width: '15rem', height: '15rem', borderRadius : '18px'}}>
+                    <img src="/uttarpradesh.png" class="card-img-top" alt="..." style={{width: '15rem', height: '7rem', borderRadius : '18px'}} />
                         <div class="card-body">
                             <h5 class="card-title">Guntur</h5>
-                            <a href="#" class="btn btn-bluish-green">Go To Map</a>
+                            <a href="#" class="btn btn-rounded btn-bluish-green">Go To Map</a>
                         </div>
                 </div>
             </div>
